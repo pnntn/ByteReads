ade40b9a23292796d968e4cd7a23b21949797852
Lucamaso16 <lucamaso16@gmail.com>
2024-04-10 17:19:10 +0200

    spring_initializer

---
+++ origanboh_spring/src/main/java/com/generation/origanboh_spring/OriganbohSpringApplication.java
@@ -0,0 +1,13 @@
+package com.generation.origanboh_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriganbohSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriganbohSpringApplication.class, args);
+	}
+
+}
