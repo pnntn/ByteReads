5af2c9e8c25ab9d3310a96c5e9af874005817fdb
pnntn <pinnaantonio1999@gmail.com>
2024-04-11 10:37:26 +0200

    Code cleanup
    Cleaning up code and removing unnecessary comments

--- origanboh_spring/src/main/java/com/generation/origanboh_spring/dao/DatabaseMysql.java
+++ origanboh_spring/src/main/java/com/generation/origanboh_spring/dao/DatabaseMysql.java
@@ -66,13 +66,6 @@ public class DatabaseMysql implements Database{
         return connection;
     }
 
-
-    //Il metodo eseguiDML() si occupa di eseguire una generica DML prendendo come parametri la query e un varargs di stringhe
-    //in modo che le query possano essere parametrizzate con il solito metodo dei placeholder ('?'). I varargs si gestiscono esattemente come se fosse un vettore
-    //equivale a scrivere un metodo con questa firma eseguiDML(String query, String[] params)
-    //ESEMPIO:
-    //query = "insert into studenti(id, matricola, id_classe) values(?, ?, ?)"
-    //params --> avrà i parametri da dover sostituire ai placeholder nell'ordine corretto
     @Override
     public int eseguiDML(String query, String... params){
         PreparedStatement ps = null;
@@ -81,8 +74,7 @@ public class DatabaseMysql implements Database{
         try{
             String[] cols = {"id"};
             ps = getConnection().prepareStatement(query, cols);
-            //Eseguo un ciclo sui parametri che mi arrivano dai varargs
-            //e rimpiazzo i placeholder con gli stessi parametri
+
             for(int i = 0; i < params.length; i++){
                 String[] split = params[i].split(":");
                 if(split[0].equals("DATA")){
@@ -114,9 +106,7 @@ public class DatabaseMysql implements Database{
         }
         catch(SQLException e){
             System.out.println("Errore nella query:\n" + query + "\n" + e.getMessage());
-            //Ritorniamo -2 perchè nel caso della insert avremo come ritorno un -1 se non si è riuscito a prendere la chiave primaria generata dal database
-            //mentre nelle update e nelle delete ritornerebbe sempre -1 perchè non stiamo generando una chiave primaria nuova, pertanto
-            //per segnalare errore ritorno un valore differente (-2)
+
             return -2;
         }
         return ris;
@@ -124,8 +114,7 @@ public class DatabaseMysql implements Database{
 
     @Override
     public Map<Integer, Map<String, String>> eseguiDQL(String query, String... params){
-        //Mi preparo una mappa che associ ad una chiave intera un'altra mappa fatta da chiavi che sono i nomi delle proprieta' mentre i valori i valori delle proprieta' stesse
-        //in modo da mappare un oggetto generico utilizzando i nomi delle colonne del db come nomi delle proprieta' e quindi come chiavi.
+
         Map<Integer, Map<String, String>> ris = new HashMap<>();
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -140,16 +129,13 @@ public class DatabaseMysql implements Database{
 
             Map<String, String> mappaProprieta;
 
-            //Ciclo le righe del ResultSet
             while(rs.next()) {
                 mappaProprieta = new HashMap<>();
-                //Ciclo le colonne del ResultSet utilizzando i metadati (quantita' di colonne presenti e nomi delle colonne stesse)
-                for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++){
-                    //Come chiave prendo il nome della colonna in posizione i, e modifico il nome affinche' non abbia underscore e che sia tutto minuscolo
-                    //data_nascita --> datanascita
-                    String chiave = rs.getMetaData().getColumnName(i).toLowerCase().replace("_", "");
 
-                    //Il valore lo prendo da ResultSet come stringa utilizzando ancora la poszione della colonna i
+                for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++){
+
+                    String chiave = rs.getMetaData().getColumnName(i).toLowerCase().replace("_", "");
+                    
                     String valore = "";
                     if("DATE".equalsIgnoreCase(rs.getMetaData().getColumnTypeName(i)))
                     {
