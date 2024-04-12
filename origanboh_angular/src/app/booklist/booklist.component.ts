70552d8f2d08a57e5778b4e0415ed347f883cf3e
LorenzoFG95 <lorenzofraterrigo@gmail.com>
2024-04-12 17:29:36 +0200

    feat: render books in homepage

--- origanboh_angular/src/app/booklist/booklist.component.ts
+++ origanboh_angular/src/app/booklist/booklist.component.ts
@@ -1,10 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Libro } from 'src/models/Libro';
 
 @Component({
   selector: 'app-booklist',
   templateUrl: './booklist.component.html',
-  styleUrls: ['./booklist.component.css']
+  styleUrls: ['./booklist.component.css'],
 })
 export class BooklistComponent {
+  libri?: Libro[];
 
+  constructor(private http: HttpClient) {
+    this.http = http;
+    this.getAllClassi();
+  }
+
+  getAllClassi() {
+    let token = 'ADMIN-1';
+    //TODO- STO USANDO UN TOKEN TEMPORANEO SOLO PER FARLO PARTIRE INTANTO
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      token: token,
+    });
+    this.http
+      .get<Libro[]>('http://localhost:8080/api/libro/all', { headers })
+      .subscribe((risposta) => {
+        this.libri = risposta;
+      });
+  }
 }
