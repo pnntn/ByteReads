07a04608c9fa8f4236695ff1eda981c5eadfa385
pnntn <pinnaantonio1999@gmail.com>
2024-04-15 16:48:54 +0200

    Added routes for gestione prodotti e clienti in dev-mode template

--- origanboh_angular/src/app/app-routing.module.ts
+++ origanboh_angular/src/app/app-routing.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookdetailComponent } from './bookdetail/bookdetail.component';
 import { CustomerareaComponent } from './customerarea/customerarea.component';
+import { DevModeComponent } from './dev-mode/dev-mode.component';
+import { GestioneUtentiComponent } from './dev-mode/gestione-utenti/gestione-utenti.component';
+import { GestioneProdottiComponent } from './dev-mode/gestione-prodotti/gestione-prodotti.component';
 
 const routes: Routes = [
   {
     path: 'dettaglio',
     component: BookdetailComponent,
   },
-  { path: 'area-admin', component: BookdetailComponent },
+  { 
+    path: 'area-admin', 
+    component: DevModeComponent },
+  {
+    path: 'gestione-prodotti',
+    component : GestioneProdottiComponent
+  },
+  {
+    path: 'gestione-utenti',
+    component: GestioneUtentiComponent,
+  },
   {
     path: 'area-cliente',
     component: CustomerareaComponent,
