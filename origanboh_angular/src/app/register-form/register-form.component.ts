afd671da0c4acbb88a517694b575bd6abd3a63b1
Lucamaso16 <lucamaso16@gmail.com>
2024-04-17 09:37:33 +0200

    piccoli fix

--- origanboh_angular/src/app/register-form/register-form.component.ts
+++ origanboh_angular/src/app/register-form/register-form.component.ts
@@ -13,7 +13,7 @@ export class RegisterFormComponent {
 
   formInserisciCliente : FormGroup;
 
-  isInserisciCliente = false;
+  isInserisciCliente = true;
 
   constructor(private http : HttpClient, private formBuilder : FormBuilder){
     this.http = http;
