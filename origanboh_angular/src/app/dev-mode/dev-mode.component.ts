7732512dbf1177a22d5e0de315ed490f3d3a365c
pnntn <pinnaantonio1999@gmail.com>
2024-04-18 11:45:37 +0200

    Checklogin on adminarea implemented

--- origanboh_angular/src/app/dev-mode/dev-mode.component.ts
+++ origanboh_angular/src/app/dev-mode/dev-mode.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Utente } from 'src/models/Utente';
 
 @Component({
   selector: 'app-dev-mode',
@@ -6,5 +8,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./dev-mode.component.css']
 })
 export class DevModeComponent {
+
+  utente?: Utente;
+
+  constructor(private http : HttpClient){
+    this.http = http;
+    this.checkLogin();
+  }
+
+  checkLogin(){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      alert("NON HAI EFFETTUATO UN LOGIN VALIDO")
+      sessionStorage.clear();
+      window.location.href="/";
+      return;
+    }
+    else{
+      const headers = new HttpHeaders(
+        {
+          'Content-Type' : 'application/json',
+          'token' : token,
+          'role' : 'ADMIN'
+        }
+      );
+
+
+      this.http.get("http://localhost:8080/api/login/checklogin", {headers}).subscribe(risposta =>{
+        let check = risposta as boolean;
+        if(!check){
+          alert("Non sei autorizzato ad accedere a questa pagina");
+          window.location.href="/";
+        }
+        else{
+          let id  = token?.split("-")[1] as string;
+          this.getUtente(id);
+        }
+      })
+
+    }
+  }
+
+  getUtente(id : string){
+    let token = sessionStorage.getItem("token");
+
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token' : token
+      }
+    )
+
+    const params = new HttpParams().set('idCliente', id);
+
+    this.http.get("http://localhost:8080/api/cliente/byId", {headers, params},).subscribe(risposta =>{
+      console.log(risposta);
+    
+      this.utente = risposta as Utente;
+
+      console.log(this.utente);
+    })
+
+  }
 
 }
--- origanboh_angular/src/app/dev-mode/gestione-utenti/gestione-utenti.component.ts
+++ origanboh_angular/src/app/dev-mode/gestione-utenti/gestione-utenti.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Cliente } from 'src/models/Cliente';
+import { Utente } from 'src/models/Utente';
 
 @Component({
   selector: 'app-gestione-utenti',
@@ -11,9 +12,11 @@ import { Cliente } from 'src/models/Cliente';
 export class GestioneUtentiComponent {
   @Input() clienti? : Cliente[];
 
+  utente?: Utente;
+
   constructor(private http : HttpClient, private formBuilder : FormBuilder){
     this.http = http;
-
+    this.checkLogin();
     this.getAllClienti();
   }
 
@@ -64,4 +67,64 @@ export class GestioneUtentiComponent {
   searchCallback(clienti : Cliente[]){
     this.clienti = clienti;
   }
+
+  checkLogin(){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      alert("NON HAI EFFETTUATO UN LOGIN VALIDO")
+      sessionStorage.clear();
+      window.location.href="/";
+      return;
+    }
+    else{
+      const headers = new HttpHeaders(
+        {
+          'Content-Type' : 'application/json',
+          'token' : token,
+          'role' : 'ADMIN'
+        }
+      );
+
+
+      this.http.get("http://localhost:8080/api/login/checklogin", {headers}).subscribe(risposta =>{
+        let check = risposta as boolean;
+        if(!check){
+          alert("Non sei autorizzato ad accedere a questa pagina");
+          window.location.href="/";
+        }
+        else{
+          let id  = token?.split("-")[1] as string;
+          this.getUtente(id);
+        }
+      })
+
+    }
+  }
+
+  getUtente(id : string){
+    let token = sessionStorage.getItem("token");
+
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token' : token
+      }
+    )
+
+    const params = new HttpParams().set('idCliente', id);
+
+    this.http.get("http://localhost:8080/api/cliente/byId", {headers, params},).subscribe(risposta =>{
+      console.log(risposta);
+    
+      this.utente = risposta as Utente;
+
+      console.log(this.utente);
+    })
+
+  }
+
 }
